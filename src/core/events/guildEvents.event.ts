8e43243dc4bdb0fc5ab38a8c8187e79796778a1f
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-12-05 12:36:16 -0300

    fix: fix build issues

--- src/core/events/guildEvents.event.ts
+++ src/core/events/guildEvents.event.ts
@@ -3,14 +3,14 @@ import { Events } from 'discord.js';
 // import { GuildServices } from '../../services/guild.service';
 import { client } from '../discord/client.discord';
 
-client.on(Events.GuildCreate, async (guild) => {
+client.on(Events.GuildCreate, async (_guild) => {
   // await GuildServices.CreateOrUpdate({
   //   id: guild.id,
   // });
   // console.log("guild_guild::create", guild);
 });
 
-client.on(Events.GuildDelete, async (guild) => {
+client.on(Events.GuildDelete, async (_guild) => {
   // await GuildServices.Delete({
   //   id: guild.id,
   // });
